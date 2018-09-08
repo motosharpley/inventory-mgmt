088b3f20a0cf74109078514700e8a6921b218c62
Bryan <motosharpley@gmail.com>
2018-09-08 17:03:09 -0500

    add mongoose connect to mongoDB

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const mongoose = require('mongoose');
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/test');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, "connection error"));
+db.once('open', function() {
+  console.log('Connected to mongodb');
+});
+
 
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600 })
+  win = new BrowserWindow({ width: 800, height: 600 });
 
   // and load the index.html of the app.
-  win.loadFile('index.html')
-}
+  win.loadFile('index.html');
+};
 
-app.on('ready', createWindow)
+// Init app window
+app.on('ready', createWindow);
