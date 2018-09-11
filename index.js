fd5a5fc64c0ffac16ce90da60b2ad8d92ac276a8
Bryan <motosharpley@gmail.com>
2018-09-11 13:34:07 -0500

    mock up add part modal

---
+++ index.js
@@ -0,0 +1,9 @@
+const BrowserWindow = require('electron').remote.BrowserWindow;
+
+let addPart = document.getElementById('addPart');
+addPart.addEventListener('click', function (e) {
+	let modal = new BrowserWindow({ width: 400, height: 600, modal: true });
+
+	// and load the index.html of the app.
+	modal.loadFile('addPart.html');
+});
